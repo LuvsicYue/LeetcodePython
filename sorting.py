586da27844009de7b0d822a47dec8a7079d26852
Chenshuo Yue <cyue@purestorage.com>
2021-07-19 16:35:01 -0700

    Updateing Sorting.py

--- sorting.py
+++ sorting.py
@@ -1,3 +1,4 @@
+import math
 from typing import List
 
 
@@ -54,3 +55,73 @@ def selection_sort2(nums: List[int]) -> List[int]:
                 min_index = j
         nums[min_index], nums[i] = nums[i], nums[min_index]
     return nums
+
+
+def insertion_sort1(nums):
+    n = len(nums)
+    for i in range(n): # Effective starting from n = 1
+        pre_index = i - 1
+        current = nums[i]
+        while pre_index >= 0 and nums[pre_index] > current:
+            nums[pre_index + 1] = nums[pre_index]
+            pre_index -= 1
+    return nums
+
+
+def insertion_sort2(nums):
+    n = len(nums)
+    for i in range(n): # Effective starting from n = 1
+        j = i
+        while j > 0 and nums[j] < nums[j - 1]:
+            nums[j], nums[j - 1] = nums[j - 1], nums[j]
+            j -= 1
+    return nums
+
+
+def shell_sort1(nums: List[int]) -> List[int]:
+    gap = len(nums) // 2
+    while gap > 0:
+        for i in range(gap, len(nums)): # insertion sort part
+            current = nums[i]
+            pre_index = i - gap
+            while pre_index >= 0 and nums[pre_index] > current:
+                nums[pre_index + gap] = nums[pre_index]
+                pre_index -= gap
+        gap //= 2
+    return nums
+
+def shell_sort2(nums):
+    n = len(nums)
+    gap = n // 2
+    while gap > 0:
+        for i in range(gap):
+            for j in range(gap + i, n, gap):
+                while j > i and nums[j] < nums[j - gap]:
+                    nums[j], nums[j - gap] = nums[j - gap], nums[j]
+                    j -= gap
+        gap //= 2
+    return nums
+
+
+def merge_sort():
+    return
+
+
+def quick_sort():
+    return
+
+
+def heap_sort():
+    return
+
+
+def counting_sort():
+    return
+
+
+def bucket_sort():
+    return
+
+
+def radixSort():
+    return
